c2473d452a2eb3ac4e37448d74e823bf52b9d160
lea.kim <lea.kim@epita.fr>
2022-05-16 13:57:33 +0200

    update

--- GUI/GUI.c
+++ GUI/GUI.c
@@ -34,11 +34,13 @@ int gui(int argc, char** argv)
 	decodepage = GTK_WIDGET(gtk_builder_get_object(builder, "decodepage"));
 	button_next_de = GTK_WIDGET(gtk_builder_get_object(builder, "button_next_de"));
 	button_back_de = GTK_WIDGET(gtk_builder_get_object(builder, "button_back_de"));
+	url = GTK_WIDGET(gtk_builder_get_object(builder, "url"));
 
 	encodepage = GTK_WIDGET(gtk_builder_get_object(builder, "encodepage"));
 	button_next_en = GTK_WIDGET(gtk_builder_get_object(builder, "button_next_en"));
 	button_back_en = GTK_WIDGET(gtk_builder_get_object(builder, "button_back_en"));
-
+	image = GTK_WIDGET(gtk_builder_get_object(builder, "image"));
+	
 
 	//Connects event handlers.
 	//g_signal_connect(button_next, "clicked", G_CALLBACK(next_fixed), NULL);
@@ -48,8 +50,8 @@ int gui(int argc, char** argv)
 	g_signal_connect(button_back_qr, "clicked", G_CALLBACK(on_button_back_clicked), &qrcodepage, &mainpage);
 	g_signal_connect(button_back_de, "clicked", G_CALLBACK(on_button_back_clicked), &decodepage, &qrcodepage);
 	g_signal_connect(button_back_en, "clicked", G_CALLBACK(on_button_back_clicked), &encodepage, &qrcodepage);
-	g_signal_connect(button_next_de, "clicked", G_CALLBACK(on_button_next_clicked), &qrcodepage, &decodepage);
-	g_signal_connect(button_next_en, "clicked", G_CALLBACK(on_button_next_clicked), &qrcodepage, &encodepage);
+	g_signal_connect(button_next_de, "clicked", G_CALLBACK(on_button_next_de_clicked), &qrcodepage, &decodepage);
+	g_signal_connect(button_next_en, "clicked", G_CALLBACK(on_button_next_en_clicked), &qrcodepage, &encodepage);
 
 	gtk_widget_show(&mainpage);
 
@@ -59,10 +61,22 @@ int gui(int argc, char** argv)
 
 }
 
-void on_button_next_clicked(GtkWidget *before, GtkWidget *after)
+void on_button_next_de_clicked(GtkWidget *before, GtkWidget *after, char path[])
 {
 	gtk_widget_hide(before);
 	gtk_widget_show(after);
+	url = "";
+	// url is the output 
+	GtkTextBuffer *buf = gtk_text_view_get_buffer(GTK_TEXT_VIEW(url));
+	// gtk_text_buffer_set_text(buf, url);
+	
+}
+
+void on_button_next_en_clicked(GtkWidget *before, GtkWidget *after, )
+{
+	gtk_widget_hide(before);
+	gtk_widget_show(after);
+	// gtk_image_set_from_file(image, path);
 }
 
 void on_button_back_clicked(GtkWidget *before, GtkWidget *after)
